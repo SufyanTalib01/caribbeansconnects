0cac1b8de8b1491d05d73cb73fbf7f0e6eb5a528
Muhammad Sufyan <sufyantalib125@gmail.com>
2024-10-10 17:17:46 +0500

    first commit

---
+++ components/creativity-category.php
@@ -0,0 +1,31 @@
+<div class="content-categories const-padding">
+                <div class="category">
+                    <img src="assets/img/content-category/actor.png" alt="">
+                    <p>ACTOR</p>
+                </div>
+                <div class="category">
+                    <img src="assets/img/content-category/musical-note.png" alt="">
+                    <p>MUSICIANS</p>
+                </div>
+                <div class="category">
+                    <img src="assets/img/content-category/microphone.png" alt="">
+                    <p>VOICEOVER</p>
+                </div>
+                <div class="category">
+                    <img src="assets/img/content-category/model-1.png" alt="">
+                    <p>MODELS</p>
+                </div>
+                <div class="category">
+                    <img src="assets/img/content-category/camera.png" alt="">
+                    <p>Crew</p>
+                </div>
+                <div class="category">
+                    <img src="assets/img/content-category/video.png" alt="">
+                    <p>Videographers</p>
+                </div>
+                <div class="category">
+                    <img src="assets/img/content-category/creative.png" alt="">
+                    <p>Content Creators
+                    </p>
+                </div>
+</div>
